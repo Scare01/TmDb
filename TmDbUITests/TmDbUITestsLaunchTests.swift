aa4b914a674a1dacd8c00f042ad27420857e6226
Слесарев Алексей <slesarev.aleksej@fitauto.ru>
2023-01-21 08:28:56 +0300

    Initial Commit

---
+++ TmDbUITests/TmDbUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  TmDbUITestsLaunchTests.swift
+//  TmDbUITests
+//
+//  Created by Алексей Слесарев on 21.01.2023.
+//
+
+import XCTest
+
+final class TmDbUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
